52a717a6aca320c0c25fd54555a67f31bb14b2dc
KNOWIT\oskkar2 <oskar.karbownik@knowi.eu>
2024-01-27 13:11:42 +0100

    Added asp.net core identity

--- QuizMoon.Client/Startup.cs
+++ QuizMoon.Client/Startup.cs
@@ -12,7 +12,10 @@ public static class Startup
 {
     public static void ConfigureServices(IServiceCollection services)
     {
+        // DB contexts
         services.AddDbContext<AppDbContext>();
+        services.AddDbContext<UserContext>();
+
         services.AddAutoMapper(typeof(MappingProfile));
      
         services.AddScoped<IFlashcardRepository, FlashcardRepository>();
---
+++ QuizMoon.DA/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuizMoon.Models.Entities;
+
+namespace QuizMoon.DA;
+
+public class UserContext(DbContextOptions<UserContext> options) : IdentityDbContext<User>(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=QuizMoon;Integrated Security=true;Encrypt=true;TrustServerCertificate=true;");
+    }
+}
